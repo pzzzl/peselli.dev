d40d7a587aa6f2543d7c08144cb70f7aabd03efb
Bruno Peselli <brunopeselli@gmail.com>
2021-06-26 21:55:04 -0300

    About!

--- script.js
+++ script.js
@@ -62,3 +62,13 @@ function myFunction(x) {
     closeNav();
   }
 }
+
+function about() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("about").style.display = "block";
+}
+
+function home() {
+  document.getElementById("home").style.display = "block";
+  document.getElementById("about").style.display = "none";
+}
