457e30c2a671697dee7edca2567021f847c3f29b
Bruno Peselli <brunopeselli@gmail.com>
2021-06-26 21:16:56 -0300

    fixes

--- script.js
+++ script.js
@@ -45,13 +45,13 @@ function standardTheme() {
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "150px";
-  document.getElementById("main").style.marginLeft = "150px";
+  document.getElementById("main").style.marginRight = "150px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("main").style.marginRight = "0";
 }
 
 function myFunction(x) {
